64eee4efaafb4bbbf331d8224be89aadf085cff3
AndiKod <andikod@proton.me>
2025-02-21 19:38:57 +0100

    Studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false  },
   extends: ['shadcn-docs-nuxt'],
   compatibilityDate: '2024-07-06',
+  modules: [
+    '@nuxthq/studio'
+  ]
 });
